923e453c3d07e51e58f8047cf7789938a4326217
Kurt Kanzenbach <kurt@linutronix.de>
2026-01-01 12:28:00 +0100

    ptr_chasing: Keep local functions static
    Keep local functions static. Re-arrange to avoid adding prototypes.
    
    Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>
    Reviewed-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- tests/workloads/pointer_chasing/pointer_chasing.c
+++ tests/workloads/pointer_chasing/pointer_chasing.c
@@ -13,6 +13,64 @@
 
 ptr_chaser_t *ptr_chaser;
 
+/* Get random int in a range of [0, max) */
+static int random_int(int max)
+{
+	return rand() % max;
+}
+
+static ptr_node *create_linked_list(void)
+{
+	ptr_node *ptr, *head;
+	uint64_t i, nr_nodes;
+	int offset;
+
+	nr_nodes = ptr_chaser->span / sizeof(ptr_node);
+
+	/* Allocate large buffer that we will read randomly from */
+	ptr_node *mem = (ptr_node *)malloc(ptr_chaser->buff * sizeof(ptr_node));
+	if (!mem) {
+		log_message(LOG_LEVEL_INFO,
+			    "[pointer_chasing]: Memory allocation failed. Consider reducing size "
+			    "of the buffer?\n");
+		return NULL;
+	}
+
+	head = &mem[random_int(ptr_chaser->buff)];
+	head->val = 1;
+	ptr = head;
+
+	/* Create linked list that will fill a buffer */
+	i = 1;
+	while (i < nr_nodes) {
+		/* Pick a random address in the span */
+		offset = random_int(nr_nodes);
+		if (mem[offset].val == 0) {
+			ptr->next = &mem[offset];
+			ptr = ptr->next;
+			ptr->val = 1;
+			i++;
+		}
+	}
+
+	return head;
+}
+
+static int generate_linked_list(ptr_chaser_t *ptr_chaser, unsigned int seed)
+{
+	ptr_node *head;
+
+	srand(seed);
+	head = create_linked_list();
+	if (!head) {
+		log_message(LOG_LEVEL_INFO, "[pointer_chasing]: Creating Linked List failed.\n");
+		return -ENOMEM;
+	}
+
+	ptr_chaser->head = (void *)head;
+	return 0;
+}
+
 int ptr_chase_setup(int argc, char *argv[])
 {
 	uint64_t buff, span;
@@ -60,64 +118,6 @@ int run_ptr_chasing(int argc, char *argv[])
 	__ptr_chasing_run_workload(ptr_chaser);
 
 	return 0;
-}
-
-int generate_linked_list(ptr_chaser_t *ptr_chaser, unsigned int seed)
-{
-	ptr_node *head;
-
-	srand(seed);
-	head = create_linked_list();
-	if (!head) {
-		log_message(LOG_LEVEL_INFO, "[pointer_chasing]: Creating Linked List failed.\n");
-		return -ENOMEM;
-	}
-
-	ptr_chaser->head = (void *)head;
-	return 0;
-}
-
-/* Get random int in a range of [0, max) */
-int random_int(int max)
-{
-	return rand() % max;
-}
-
-ptr_node *create_linked_list(void)
-{
-	ptr_node *ptr, *head;
-	uint64_t i, nr_nodes;
-	int offset;
-
-	nr_nodes = ptr_chaser->span / sizeof(ptr_node);
-
-	/* Allocate large buffer that we will read randomly from */
-	ptr_node *mem = (ptr_node *)malloc(ptr_chaser->buff * sizeof(ptr_node));
-	if (!mem) {
-		log_message(LOG_LEVEL_INFO,
-			    "[pointer_chasing]: Memory allocation failed. Consider reducing size "
-			    "of the buffer?\n");
-		return NULL;
-	}
-
-	head = &mem[random_int(ptr_chaser->buff)];
-	head->val = 1;
-	ptr = head;
-
-	/* Create linked list that will fill a buffer */
-	i = 1;
-	while (i < nr_nodes) {
-		/* Pick a random address in the span */
-		offset = random_int(nr_nodes);
-		if (mem[offset].val == 0) {
-			ptr->next = &mem[offset];
-			ptr = ptr->next;
-			ptr->val = 1;
-			i++;
-		}
-	}
-
-	return head;
 }
 
 __attribute__((destructor)) int ptr_chaser_finish(void)
--- tests/workloads/pointer_chasing/pointer_chasing.h
+++ tests/workloads/pointer_chasing/pointer_chasing.h
@@ -47,9 +47,6 @@ static __attribute__((always_inline)) inline void __ptr_chasing_run_workload(
 			     :);
 }
 
-int generate_linked_list(ptr_chaser_t *ptr_chaser, unsigned int seed);
-union ptr_node *create_linked_list(void);
-
 /* Setup function */
 int ptr_chase_setup(int argc, char *argv[]);
 
